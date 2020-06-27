ee438949ef18ffa4666ca65514a00a798ee36525
jorgepasco1 <jorgepascosoto@gmail.com>
2020-06-27 01:40:04 -0500

    remove js style object

--- src/Form.js
+++ src/Form.js
@@ -2,31 +2,6 @@ import React from "react";
 import Modal from "react-modal";
 import "./styles/Form.css"
 
-const customStyles = {
-  overlay: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "rgb(75, 75, 75,0.75)",
-  },
-  content: {
-    fontFamily: "Montserrat",
-    position: "absolute",
-    top: "20%",
-    left: "30%",
-    right: "30%",
-    bottom: "20%",
-    background: "whitesmoke",
-    overflow: "auto",
-    WebkitOverflowScrolling: "touch",
-    borderRadius: "10px",
-    outline: "none",
-    padding: "5%",
-  },
-};
-
 Modal.setAppElement('#root');
 
 export default function Form(props) {
