efe2725b99ca9f1dd7767bbdef0d96011ac706b9
jorgepasco1 <jorgepascosoto@gmail.com>
2020-06-27 13:37:17 -0500

    finish form interaction

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Modal from "react-modal";
 import "./styles/Form.css";
 
@@ -9,6 +9,17 @@ export default function Form(props) {
   const [userEmail, setUserEmail] = useState("");
   const [userMessage, setUserMessage] = useState("");
 
+  const formRef =  useRef(null)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formRef.current.checkValidity()) {
+      console.log(userName)
+    } else {
+      formRef.current.reportValidity()
+    }
+  };
+
   return (
     <Modal
       isOpen={props.isOpen}
@@ -18,7 +29,7 @@ export default function Form(props) {
       contentLabel="Contact Modal"
     >
       <h2 className="form-title">Contact Me</h2>
-      <form>
+      <form ref={formRef}>
         <div className="inline-inputs">
           <div className="input-container">
             <label htmlFor="your-name">Your name</label>
@@ -27,23 +38,40 @@ export default function Form(props) {
               name="your-name"
               value={userName}
               onChange={(event) => setUserName(event.target.value)}
+              required
             />
           </div>
           <div className="input-container">
             <label htmlFor="email">Your email</label>
-            <input className="form-input" name="email" value={userEmail} onChange={(event) => setUserEmail(event.target.value)} />
+            <input
+              className="form-input"
+              name="email"
+              type="email"
+              value={userEmail}
+              onChange={(event) => setUserEmail(event.target.value)}
+              required
+            />
           </div>
         </div>
         <textarea
           className="form-ta"
           name="form-ta"
-          placeholder="Start typing your message"
+          placeholder={
+            userName && userEmail
+              ? "Start typing your message"
+              : "Please, fill out name and email first."
+          }
           rows="15"
           cols="70"
-          value={userMessage} onChange={(event) => setUserMessage(event.target.value)}
+          readOnly={!(userName && userEmail)}
+          value={userMessage}
+          onChange={(event) => setUserMessage(event.target.value)}
+          required
         ></textarea>
+        <button onClick={handleSubmit} className="submit-button">
+          Send
+        </button>
       </form>
-      <button className="submit-button">Send</button>
       <button
         className="unstyled-button close-button"
         onClick={props.closeModal}
