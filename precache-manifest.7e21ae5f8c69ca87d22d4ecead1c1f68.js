00f6b214f71593911b62618800235d9b0a897f06
jorgepasco1 <jorgepascosoto@gmail.com>
2020-06-27 19:23:14 -0500

    Updates

---
+++ precache-manifest.7e21ae5f8c69ca87d22d4ecead1c1f68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430c22068f8f4f29abc906ca74d211d1",
+    "url": "/web-portfolio/index.html"
+  },
+  {
+    "revision": "5d64322d0b47a1467fcb",
+    "url": "/web-portfolio/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "69d8dac5ff2298db66a1",
+    "url": "/web-portfolio/static/css/main.21e0551f.chunk.css"
+  },
+  {
+    "revision": "5d64322d0b47a1467fcb",
+    "url": "/web-portfolio/static/js/2.863fd910.chunk.js"
+  },
+  {
+    "revision": "e7c69aa4549a0938e155d41214f1973f",
+    "url": "/web-portfolio/static/js/2.863fd910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d8dac5ff2298db66a1",
+    "url": "/web-portfolio/static/js/main.d904dc64.chunk.js"
+  },
+  {
+    "revision": "abe42df77f98945b136c",
+    "url": "/web-portfolio/static/js/runtime-main.66285d81.js"
+  },
+  {
+    "revision": "0031d183886d55291d1d4434a1b10aaa",
+    "url": "/web-portfolio/static/media/profile.0031d183.png"
+  }
+]);
